143f92d552c38b8d5eabc03d127a8c2736cbeef2
lawci <lcft1204@gmail.com>
2025-06-22 23:59:59 +0800

    final commit

--- src/TheaterKiosk.java
+++ src/TheaterKiosk.java
@@ -1,22 +1,37 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public class TheaterKiosk
+{
+    public static void main(String[] args)
+    {
+        //class GetWristband
+        //main ()
+        //  num age
+        //  output “Enter your age: ”
+        //  input age
+        //  if age >= 21 then
+        //     output “You get a wrist band.”
+        //     end if
+        //   return
+        //end class
+
         Scanner in = new Scanner(System.in);
         int age;
         String trash;
 
-        // Prompt user for age
         System.out.print("Enter your age: ");
-        if (in.hasNextInt()) {
+        if (in.hasNextInt())
+        {
             age = in.nextInt();
-            in.nextLine(); // Clear buffer
+            in.nextLine();
 
-            // Check if 21 or older
-            if (age >= 21) {
-                System.out.println("You get a wrist band!");
+            if (age >= 21)
+            {
+                System.out.println("You get a wrist band.");
             }
-        } else {
+        }
+        else
+        {
             trash = in.nextLine();
             System.out.println("Invalid input: " + trash + ". Please enter an integer.");
         }
